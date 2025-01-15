1338f6e545c96c8071ea3065eb5c676ab04ea3fe
zafaraftab1 <zafaraftab14581@gmail.com>
2025-01-15 16:16:30 +0530

    setup images from static files

--- fashionWebsite/settings.py
+++ fashionWebsite/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
