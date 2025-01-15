aaaa3ecc485ef53160de5a3497bcb8ac57684dd9
zafaraftab1 <zafaraftab14581@gmail.com>
2025-01-15 17:57:35 +0530

    Crete a view for shop

--- fashion/urls.py
+++ fashion/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home', views.home, name='home'),
+    path('shop', views.shop, name='shop'),
 ]
--- fashion/views.py
+++ fashion/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'fashionHTML/index.html')
+
+
+def shop(request):
+    return render(request,'fashionHTML/shop.html')
