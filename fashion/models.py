060551d16ec8865153ae9e0c39af08b3ea73d8a5
zafaraftab1 <zafaraftab14581@gmail.com>
2025-01-16 16:19:52 +0530

    create models and define mode class name 'fashionCategory'

--- fashion/models.py
+++ fashion/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class fashionCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    #image = models.ImageField(upload_to='fashionCategory/')
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return self.name
+
