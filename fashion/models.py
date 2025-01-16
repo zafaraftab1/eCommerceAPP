24e1bc9159d22d463a65877d1206258927e4de58
zafaraftab1 <zafaraftab14581@gmail.com>
2025-01-16 21:06:40 +0530

    Setup Postgre SQL in setting and download dependency for our database

--- fashion/models.py
+++ fashion/models.py
@@ -6,6 +6,7 @@ class fashionCategory(models.Model):
     name = models.CharField(max_length=100)
     #image = models.ImageField(upload_to='fashionCategory/')
     price = models.DecimalField(decimal_places=2, max_digits=10)
+    offer = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
