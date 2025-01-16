0236a7c0b78554e0422d43d72a41a79552a2ca56
zafaraftab1 <zafaraftab14581@gmail.com>
2025-01-16 21:23:50 +0530

    Create requirement.txt file and add some dependencies in that folder

--- fashion/models.py
+++ fashion/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class fashionCategory(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    #image = models.ImageField(upload_to='fashionCategory/')
+    image = models.ImageField(upload_to='fashionCategory/')
     price = models.DecimalField(decimal_places=2, max_digits=10)
     offer = models.BooleanField(default=False)
 
